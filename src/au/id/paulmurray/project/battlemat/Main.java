17fb65ce4e40ab67873a4810dab58e9692fafef5
Paul Murray <pmurray@bigpond.com>
2012-08-04 15:27:07 +1000

    using interger points for measurement

--- src/au/id/paulmurray/project/battlemat/Main.java
+++ src/au/id/paulmurray/project/battlemat/Main.java
@@ -10,70 +10,68 @@ public class Main {
 	// 3 11.69 16.54
 	// 4 8.27 11.69
 
-	
 	static final int W = 32;
 	static final int H = 22;
+//	static final int W = 8;
+//	static final int H = 10;
 	static Random rnd = new Random();
-
 
 	static final double ZZ = .01;
 
 	static double z0(double a) {
-		return ZZ + rnd.nextDouble() * (rnd.nextInt(10)==0?ZZ:ZZ*3);
+		return ZZ + rnd.nextDouble() * (rnd.nextInt(10) == 0 ? ZZ : ZZ * 3);
 	}
 
 	static double z1(double a) {
-		return .5 - ZZ + rnd.nextDouble() *  (rnd.nextInt(10)==0?ZZ:ZZ*3) * 2;
+		return .5 - ZZ + rnd.nextDouble() * (rnd.nextInt(10) == 0 ? ZZ : ZZ * 3) * 2;
 	}
 
 	static double z2(double a) {
-		return 1 - ZZ - rnd.nextDouble() *  (rnd.nextInt(10)==0?ZZ:ZZ*3);
+		return 1 - ZZ - rnd.nextDouble() * (rnd.nextInt(10) == 0 ? ZZ : ZZ * 3);
 	}
 
 	public static void main(String[] args) throws Exception {
 
-		PrintWriter w = new PrintWriter(new FileOutputStream(new File(System
-				.getProperty("user.dir"), "a1Grid.svg")));
+		PrintWriter w = new PrintWriter(new FileOutputStream(new File(System.getProperty("user.dir"), "a1Grid.svg")));
 
-		w
-				.println("<?xml version='1.0' encoding='UTF-8' ?><!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>");
+		w.println("<?xml version='1.0' encoding='UTF-8' ?><!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>");
 
 		w.println("<svg");
 		w.println("   xmlns:svg='http://www.w3.org/2000/svg'");
 		w.println("   xmlns='http://www.w3.org/2000/svg'");
 		w.println("   xmlns:xlink='http://www.w3.org/1999/xlink'");
 		w.println("   version='1.1'");
-		w.println("   width='" + (W+.1) + "in'");
-		w.println("   height='" + (H+.1) + "in'");
-		w.println("  viewBox='-.05 -.05 " + (W+.1) + " " + (H+.1) + "'");
+		w.println("   width='" + (W * 72 + 10) + "pt'");
+		w.println("   height='" + (H * 72 + 10) + "pt'");
+		w.println("  viewBox='-5 -5 " + (W * 72 + 10) + " " + (H * 72 + 10) + "'");
 		w.println("  >	");
 
-//		 w.println("<defs>");
-//		
-//		 for (int i = 0; i < 10; i++) {
-//		
-//		 w.println("<radialGradient id='MyGradient" + i
-//		 + "' gradientUnits='userSpaceOnUse'");
-//		 w.println("cx='" + (.45 + rnd.nextDouble() * .1) + "' cy='"
-//		 + (.45 + rnd.nextDouble() * .1) + "' r='.75' ");
-//		 w.println("fx='" + (.33 + rnd.nextDouble() * .4 - .2) + "' fy='"
-//		 + (.33 + rnd.nextDouble() * .4 - .2)
-//		 + "' spreadMethod='repeat'>");
-//		 w.println("<stop offset='0%' stop-color='white' />");
-//		 w.println("<stop offset='33%' stop-color='white' />");
-//		 w.println("<stop offset='50%' stop-color='#f4f4f4' />");
-//		 w.println("<stop offset='100%' stop-color='#c0c0c0' />");
-//		 w.println("</radialGradient>");
-//		 }
-//		
-//		 w.println("</defs>");
+		// w.println("<defs>");
+		//
+		// for (int i = 0; i < 10; i++) {
+		//
+		// w.println("<radialGradient id='MyGradient" + i
+		// + "' gradientUnits='userSpaceOnUse'");
+		// w.println("cx='" + (.45 + rnd.nextDouble() * .1) + "' cy='"
+		// + (.45 + rnd.nextDouble() * .1) + "' r='.75' ");
+		// w.println("fx='" + (.33 + rnd.nextDouble() * .4 - .2) + "' fy='"
+		// + (.33 + rnd.nextDouble() * .4 - .2)
+		// + "' spreadMethod='repeat'>");
+		// w.println("<stop offset='0%' stop-color='white' />");
+		// w.println("<stop offset='33%' stop-color='white' />");
+		// w.println("<stop offset='50%' stop-color='#f4f4f4' />");
+		// w.println("<stop offset='100%' stop-color='#c0c0c0' />");
+		// w.println("</radialGradient>");
+		// }
+		//
+		// w.println("</defs>");
 
-//		 w.println("<rect x='0' y='0' width='"+W+"' height='"+H+"' fill='#e8e8e8'/>");
+		// w.println("<rect x='0' y='0' width='"+W+"' height='"+H+"' fill='#e8e8e8'/>");
 
-		w.println("<g stroke='#808080' stroke-width='.007'>");
+		w.println("<g stroke='#808080' stroke-width='.5pt'>");
 		for (int x = 0; x < W; x++)
 			for (int y = 0; y < H; y++) {
-				w.print("<svg x='" + x + "' y='" + y + "'>");
+				w.print("<svg x='" + pp(x) + "' y='" + pp(y) + "'>");
 
 				int xx2 = 1 + x % 2 * 2;
 				int xx1 = 3 - x % 2 * 2;
@@ -83,39 +81,42 @@ public class Main {
 				double x1 = z0(xx1);
 				double y1 = z1(1);
 				w.print("<path" //
-					// + " fill='url(#MyGradient" + rnd.nextInt(10) + ")'" //
+						// + " fill='url(#MyGradient" + rnd.nextInt(10) + ")'"
+						// //
 						+ " fill='none'" //
-						+ " d='M " + x1 + " " + y1//
+						+ " d='M " + pp(x1) + " " + pp(y1)//
 						+ " C " //
-						+ z0(xx1) + " " + z2(yy2) + " " //
-						+ z0(xx1) + " " + z2(yy2) + " " //
-						+ z1(1) + " " + z2(yy2)//
+						+ pp(z0(xx1)) + " " + pp(z2(yy2)) + " " //
+						+ pp(z0(xx1)) + " " + pp(z2(yy2)) + " " //
+						+ pp(z1(1)) + " " + pp(z2(yy2))//
 						+ " S " //
-						+ z2(xx2) + " " + z2(yy2) + " " //
-						+ z2(xx2) + " " + z1(1)//
+						+ pp(z2(xx2)) + " " + pp(z2(yy2)) + " " //
+						+ pp(z2(xx2)) + " " + pp(z1(1))//
 						+ " S " //
-						+ z2(xx2) + " " + z0(yy1) + " " //
-						+ z1(1) + " " + z0(yy1) //
+						+ pp(z2(xx2)) + " " + pp(z0(yy1)) + " " //
+						+ pp(z1(1)) + " " + pp(z0(yy1)) //
 						+ " S " //
-						+ z0(xx1) + " " + z0(yy1) + " " //
-						+ x1 + " " + y1//
+						+ pp(z0(xx1)) + " " + pp(z0(yy1)) + " " //
+						+ pp(x1) + " " + pp(y1)//
 						+ " z'/>");
 				w.println("</svg>");
 
 			}
 		w.println("</g>");
-		w.println("<g stroke='black' stroke-width='.014'>");
+		w.println("<g stroke='black' stroke-width='1pt'>");
 		for (int x = 0; x <= W; x += 2)
 			for (int y = 0; y <= H; y += 2) {
-				w.println("<line x1='" + (x - .05) + "' y1='" + (y) + "' x2='"
-						+ (x + .05) + "' y2='" + (y) + "' />");
-				w.println("<line x1='" + (x) + "' y1='" + (y - .05) + "' x2='"
-						+ (x) + "' y2='" + (y + .05) + "' />");
+				w.println("<line x1='" + (pp(x)-5) + "' y1='" + pp(y) + "' x2='" + (pp(x)+5) + "' y2='" + pp(y) + "' />");
+				w.println("<line x1='" + pp(x) + "' y1='" + (pp(y)-5) + "' x2='" + pp(x) + "' y2='" + (pp(y)+5) + "' />");
 
 			}
 		w.println("</g>");
 		w.println("</svg>");
 		w.close();
+	}
+
+	private static int pp(double z0) {
+		return (int) (z0 * 72 + .49);
 	}
 
 }
